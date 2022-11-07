bd78274da4ef0812133c0f28436500bb5f257e18
20120230 <duytruong5437@gmail.com>
2022-11-07 12:41:59 +0700

    add header and sidebar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
+import Sidebar from '../../components/Sidebar/Sidebar';
 import "./Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+        <Header />
+        <div className='home'>
+            <Sidebar />
+        </div>
+    </>
   )
 }
 
