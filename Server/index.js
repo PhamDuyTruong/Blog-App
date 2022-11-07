b70c2d4c3557792b414cbba5e8bf84c920d27eea
20120230 <duytruong5437@gmail.com>
2022-11-07 16:32:38 +0700

    set up server

---
+++ Server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+    console.log("Server is running");
+})
