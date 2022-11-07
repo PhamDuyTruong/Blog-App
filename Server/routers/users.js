f0477042d2f8bfc41b61e4aa3b6234c5e61ade37
20120230 <duytruong5437@gmail.com>
2022-11-07 20:29:09 +0700

    update user

--- Server/routers/users.js
+++ Server/routers/users.js
@@ -3,9 +3,13 @@ const { route } = require("./auth");
 
 const router = require("express").Router();
 
+// Get user
 router.get("/", userControllers.getAllUser);
 router.get("/:id", userControllers.getUserById);
 
+// Update user
+router.put("/:id", userControllers.updateUser);
+//Delete user
 
 
 module.exports = router;
