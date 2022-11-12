ae52a65060c093b6591ea5ed14885f8c1965954e
20120230 <duytruong5437@gmail.com>
2022-11-12 10:48:15 +0700

    fix loi

--- Server/index.js
+++ Server/index.js
@@ -28,7 +28,7 @@ const storage = multer.diskStorage({
       cb(null, "images");
     },
     filename: (req, file, cb) => {
-      cb(null, file.originalname);
+      cb(null, req.body.name);
     },
   });
 
