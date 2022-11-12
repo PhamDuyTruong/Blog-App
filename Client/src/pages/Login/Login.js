1422c83b7614e8888fc4b61845cc4c140c630231
20120230 <duytruong5437@gmail.com>
2022-11-12 09:25:59 +0700

    login user

--- Client/src/pages/Login/Login.js
+++ Client/src/pages/Login/Login.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import "./Login.css"
+import React, {useRef, useContext} from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+import "./Login.css";
+import {Context} from '../../context/Context'
 
 function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const {user, dispatch, isFetching} = useContext(Context);
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    dispatch({type: "LOGIN_START"});
+    try {
+      const res= await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value
+      });
+      dispatch({type: "LOGIN_SUCCESS", payload: res.data});
+    } catch (error) {
+      dispatch({ type: "LOGIN_FAILURE" });
+    }
+
+  };
+
+  console.log(user);
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
-        <label>Email</label>
-        <input className="loginInput" type="text" placeholder="Enter your email..." />
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input className="loginInput" type="text" placeholder="Enter your username..." ref={userRef}/>
         <label>Password</label>
-        <input className="loginInput" type="password" placeholder="Enter your password..." />
-        <button className="loginButton">Login</button>
+        <input className="loginInput" type="password" placeholder="Enter your password..." ref={passwordRef}/>
+        <button className="loginButton" type='submit'  disabled={isFetching}>Login</button>
       </form>
-        <button className="loginRegisterButton">Register</button>
+        <button className="loginRegisterButton">
+        <Link className="link" to="/register">
+          Register
+        </Link>
+        </button>
     </div>
   )
 }
